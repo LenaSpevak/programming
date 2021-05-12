38db7bdd7806aba8965a4f34a9b2051ed450fd09
LenaSpevak <71040087+LenaSpevak@users.noreply.github.com>
2021-05-12 16:13:42 +0300

    Update porcelain.py

--- homework04/porcelain.py
+++ homework04/porcelain.py
@@ -10,14 +10,25 @@ from pyvcs.tree import commit_tree, write_tree
 
 def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
     # PUT YOUR CODE HERE
-    ...
+    update_index(gitdir, path)
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
     # PUT YOUR CODE HERE
-    ...
-
+    tree = write_tree(gitdir, read_index(gitdir))
+    commit = commit_tree(gitdir, tree, message, author=author)
+    return commit
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
     # PUT YOUR CODE HERE
-    ...
+    indexes = [index.name for index in read_index(gitdir)]
+    catalog = gitdir.parent
+    for obj in catalog.iterdir():
+        if obj in [gitdir.name]:
+            continue
+        if obj.name in indexes:
+            os.remove(obj)
+
+    commit = read_object(obj_name, gitdir)[1].decode()
+    tree = commit.split()[1]  #разделяет по всем нечитаемым символам
+    #tree = read_object(tree, gitdir)[1]
